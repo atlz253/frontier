e80c7ed6fa0a8d217252028cbb579af740d9728a
Fedor Alekseev <f.alexeeff2010@yandex.ru>
2025-07-27 16:22:56 +0300

    remove Module class

--- tests/module.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, test } from "vitest";
-import { Module } from "../src/module.ts";
-
-describe(Module.name, () => {
-  test("should create", () => {
-    const module = new Module();
-  });
-});
